78b6bcd5f1a97ef21a7f739d6eb95baa3a69d49d
KingSilons1223 <abdussalamaribiyo@gmail.com>
2023-10-24 20:21:25 +0100

    slap

--- liyu.js
+++ liyu.js
@@ -64,6 +64,44 @@ document.addEventListener('DOMContentLoaded', function() {
             navItems.style.display = 'block';
         }
     });
-
-    // Add any other interactivity you'd like here!
 });
+
+document.querySelectorAll('nav ul li a').forEach(anchor => {
+    anchor.addEventListener('click', function (e) {
+        e.preventDefault();
+
+        const section = document.querySelector(this.getAttribute('href'));
+        section.scrollIntoView({
+            behavior: 'smooth'
+        });
+    });
+});
+document.querySelectorAll('nav ul li a').forEach(anchor => {
+    anchor.addEventListener('click', function (e) {
+        e.preventDefault();
+
+        const section = document.querySelector(this.getAttribute('href'));
+        section.scrollIntoView({
+            behavior: 'smooth'
+        });
+    });
+});
+window.addEventListener('scroll', function() {
+    const scrolled = window.scrollY;
+    if (scrolled > 100) {
+        document.body.style.backgroundColor = '#ffd280';  // Change color according to your preference
+    } else {
+        document.body.style.backgroundColor = 'rgb(255,192, 0)';
+    }
+});
+const cards = document.querySelectorAll('.card');
+
+    cards.forEach((card) => {
+        card.addEventListener('mouseover', function() {
+            this.style.transform = 'scale(1.05) translateY(-10px)';
+        });
+
+        card.addEventListener('mouseout', function() {
+            this.style.transform = 'scale(1) translateY(0)';
+        });
+    });
